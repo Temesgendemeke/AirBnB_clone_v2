c4c3cbb6e57c11938023ebd9c2f9b115955d805e
Temesgen dora <tdemeke36@gmail.com>
2024-02-20 09:22:56 -0800

    fixed: task 4"; git push

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,6 @@
 -- Prepares the MySQL server for this project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-SET GLOBAL validate_password.policy=LOW;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-SET GLOBAL validate_password.policy=MEDIUM;
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,6 @@
--- Prepares the MySQL server for this project test 
+-- Prepares the MySQL server for this project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-SET GLOBAL validate_password.policy=LOW;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-SET GLOBAL validate_password.policy=MEDIUM;
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
