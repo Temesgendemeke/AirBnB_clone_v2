379dbd323b64182b2295c2f48affd9fccd4f9b47
Temesgen dora <tdemeke36@gmail.com>
2024-02-20 09:20:39 -0800

    fixed: task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,8 @@
+-- Prepares the MySQL server for this project test 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 SET GLOBAL validate_password.policy=LOW;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 SET GLOBAL validate_password.policy=MEDIUM;
+FLUSH PRIVILEGES;
