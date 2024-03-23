c85ca46060d6ac64a148c31103b69993c8549bdf
Temesgen dora <tdemeke36@gmail.com>
2024-03-22 23:06:59 -0700

    flask: task 3

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application:
+
+    Returns:
+        string: in html form.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    functions gets called when user inputs ip or domain name + '/'
+
+    Returns:
+        String : HTML
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hellosec():
+    """
+    functions gets called when user inputs '/hbnb'
+
+    Returns:
+        String : HTML
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """
+    functions gets called when user inputs '/c/<anytext>'
+
+    Returns:
+        String : HTML
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
