05723cf8e358a69a9c10a155b20987ca56aed9a3
Temesgen dora <tdemeke36@gmail.com>
2024-03-23 09:25:44 -0700

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""_summary_
+
+    Returns:
+        _type_: _description_
+"""
 
 
 from flask import Flask, render_template
@@ -9,10 +14,20 @@ app = Flask(__name__, template_folder="./templates")
 
 @app.route('/states_list', strict_slashes=False)
 def state_html():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     return render_template("7-states_list.html", states = storage.all("State"))
 
 @app.teardown_appcontext()
 def teardown(exc):
+    """_summary_
+
+    Args:
+        exc (_type_): _description_
+    """
     storage.close()
 
 
