fc46e5b56baf9a7029987120342e103d71ff07dd
Temesgen dora <tdemeke36@gmail.com>
2024-03-22 22:58:57 -0700

    flask: task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application:
+
+    Returns:
+        string: in html form.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    functions gets called when user inputs ip or domain name + '/'
+
+    Returns:
+        String : HTML
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hellosec():
+    """
+    functions gets called when user inputs ip or domain name + '/'
+
+    Returns:
+        String : HTML
+    """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
